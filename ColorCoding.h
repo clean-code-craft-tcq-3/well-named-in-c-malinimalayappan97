b43db6d71ac35797774dc483c63d83f0fa3bf7b1
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-13 00:37:05 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -1,5 +1,4 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
