03d883b332e1f6da41f2be1de72ea56fb00bc0e1
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-13 00:37:15 +0530

    Update ColorCodingControl.h

--- ColorCodingControl.h
+++ ColorCodingControl.h
@@ -1,1 +1,4 @@
-
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
