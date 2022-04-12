00cfb1ff083d7858747794226f10124bdd9a2d91
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-13 00:26:48 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -16,4 +16,4 @@ void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
