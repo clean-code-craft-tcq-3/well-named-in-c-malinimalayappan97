115ff7cf3b195e0bc502d9644a9480a0fede9ff8
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-12 22:49:40 +0530

    Update ColorCodingControl.c

--- ColorCodingControl.c
+++ ColorCodingControl.c
@@ -1,5 +1,10 @@
 #include "ColorCoding.h"
 
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
