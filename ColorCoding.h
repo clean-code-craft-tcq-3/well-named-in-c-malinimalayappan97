999b97bd0234fed620ed704c71b5654bb65996cd
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-13 00:39:04 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -1,3 +1,5 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 const char* MajorColorNames[] = {
@@ -15,3 +17,5 @@ void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
