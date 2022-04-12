3739413e92dbf7946e699704e60811ef297cf857
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-12 22:06:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-//const int MAX_COLORPAIR_NAME_CHARS = 16;
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
