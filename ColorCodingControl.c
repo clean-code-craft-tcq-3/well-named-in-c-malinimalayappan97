d5989cac78e9c0522eac2b7b182cf42a0a16196b
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-13 00:43:35 +0530

    Update ColorCodingControl.c

--- ColorCodingControl.c
+++ ColorCodingControl.c
@@ -2,6 +2,14 @@
 #include <assert.h>
 #include "ColorCoding.h"
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
