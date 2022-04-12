d82989bd94cf09d965c3b7a40be46ecd66fa15e7
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-04-12 22:00:14 +0530

    Create ColorCoding.h

---
+++ ColorCoding.h
@@ -0,0 +1,13 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
